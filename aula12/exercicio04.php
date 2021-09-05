140d4d65fff133f844102fab3d11436c27eda03f
laribrito <lary.29.ds@gmail.com>
2021-09-05 15:35:39 -0300

    Resoluçao do exercicio da aula 12 e correçao no CSS
    O ultimo exercicio da aula 12 foi respondido e o CSS da pagina
    foi atualizado para que a tag <a> se pareça mais com o botao do
    projeto.

--- aula12/exercicio04.php
+++ aula12/exercicio04.php
@@ -9,17 +9,17 @@
 <body>
 	<?php
 		$num = isset($_POST["num"])? $_POST["num"]:1;
-		echo "$num! = ";
-		$fat = $num;
-		do {
-			echo "$num x ";
-			$num--;
-			$fat = $fat * $num;
-		} while ($num >=2);
-		echo "1 = $fat";
+		echo "<h1>Tabuada do $num</h1>";
+		echo "<div style='margin-left: 40px'>";
+		$x = 1;
+		while ($x<=10){
+			echo "<p>$num x $x = ". ($num*$x). "</p>";
+			$x++;
+		}
+		echo "</div>"
 	?>
 	<br><br>
-    <a href="exercicio03.html" style="background-color: white; color: black; padding: 5px; margin: 20px">Voltar</a>
+    <a href="exercicio04-parte01.php">Voltar</a>
 </div>
 </body>
 </html>
